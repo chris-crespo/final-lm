942ba96a81bfe876efb152e5f204ab8f36aab366
krs98 <christian.crespoo98@gmail.com>
2022-04-13 16:33:21 +0200

    Update server

--- server.js
+++ server.js
@@ -1,63 +1,51 @@
 const http = require("http");
 const fs   = require("fs");
+const { exec } = require("child_process");
 
-const serveTo = (res, code = 200) => (name, type) => {
-    res.writeHead(code, { "content-type": type });
+const dirs = ["out", "pages", "styles", "assets"];
+
+const serveAs = (name, type) => res => {
+    res.writeHead(200, { "content-type": type });
     fs.createReadStream(name).pipe(res);
 }
 
-const removeExt = file => file.split(".")[0];
-const removeDir = path => path.substring(path.indexOf("/") + 1);
-
-const toRoute = (type, fn = x => x) => serve => file =>
-    ([`/${fn(file)}`, () => serve(file, type)]);
-
-const toHtmlRoute  = toRoute("text/html", f => removeDir(removeExt(f)));
-const toStyleRoute = toRoute("text/css");
-const toJsRoute    = toRoute("text/javascript");
-const toImgRoute   = serve => file => {
-    const ext = file.split(".").at(-1);
-    return toRoute(`image/${ext}`)(serve)(file);
+const fileTypes = {
+    html: "text/html",
+    css:  "text/css",
+    js:   "text/javascript",
+    png:  "image/png",
+    jpg:  "image/jpg",
+    mp4:  "video/mp4"
 }
+const last = arr => arr[arr.length - 1];
+const ext  = file => last(file.split("."));
+const fileType = file => fileTypes[ext(file)];
 
-const toMp4Route = serve => file => 
-    toRoute("video/mp4")(serve)(file);
-
-const isFile = item => item.split(".").length != 1;
+const isDir = path => fs.lstatSync(path).isDirectory();
 const readDir = dir => 
     fs.readdirSync(dir)
         .map(item => `${dir}/${item}`)
-        .flatMap(item => isFile(item) ? item : readDir(item));
+        .flatMap(item => isDir(item) ? readDir(item) : item);
 
-const createRoutes = (dir, toRoute) => serve => {
-    const files = readDir(dir);
-    return Object.fromEntries(files.map(toRoute(serve)));
-}
+const stripIndex = str => str.replace("index.html", "");
+const stripHtmlExt = str => str.replace(".html", "");
+const transformHtml = str => stripHtmlExt(stripIndex(str));
 
-const createHtmlRoutes  = createRoutes("pages",        toHtmlRoute);
-const createStyleRoutes = createRoutes("styles",       toStyleRoute);
-const createImgRoutes   = createRoutes("assets/img",   toImgRoute);
-const createMp4Routes   = createRoutes("assets/video", toMp4Route);
-const createJsRoutes    = createRoutes("src",          toJsRoute);
+const transform = file => 
+    ext(file) === "html" ? transformHtml(last(file.split("/"))) : file;
 
-const notFound = res => serveTo(res, 404)("pages/404.html", "text/html");
+const toEntry = file => ["/" + transform(file), serveAs(file, fileType(file))];
+const createEntries = dirs => 
+    dirs.filter(fs.existsSync)
+        .flatMap(readDir)
+        .map(toEntry);
+
+const createRoutes = dirs => Object.fromEntries(createEntries(dirs));
+const routes = createRoutes(dirs);
 
 const server = http.createServer((req, res) => {
-    const serveFile = serveTo(res);
-
-    const routes = {
-        "/":            () => serveFile("index.html", "text/html"),
-        "/favicon.ico": () => {},
-        ...createHtmlRoutes(serveFile),
-        ...createStyleRoutes(serveFile),
-        ...createImgRoutes(serveFile),
-        ...createMp4Routes(serveFile),
-        ...createJsRoutes(serveFile)
-    };
-
     const route = routes[req.url];
-    route();
-    //route ? route() : notFound(res);
+    route && route(res);
 });
 
 server.listen(process.env.PORT || 3000, () => console.log("Listening at port 3000..."));
