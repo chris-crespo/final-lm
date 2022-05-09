27e4c7daaaf55a9b8fe026bf7ca408f277014bfe
Chris <christian.crespoo98@gmail.com>
2022-05-09 17:28:53 +0200

    improve nav styles

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,18 +1,33 @@
-const Nav = () => (
-    <nav>
-        <ul>
-            <li id="navLogo">
-                <img src="../assets/img/logoC.png" alt="Nav logo" />
-            </li>
-        </ul>
-        <ul>
-            <li><a href="">HOME</a></li>
-            <li><a href="">CAMPS</a></li>
-            <li><a href="">ABOUT</a></li>
+const NavLink = ({ name, to }) => (
+    <li className="navbar-menu-item">
+        <a href={to}>{ name }</a>
+        <div className="menu-item-line" />
+    </li>
+)
 
-            <li id="signin"><a href="/signin">SignIn</a></li>
-            <li id="signup"><a href="/signup">SignUp</a></li>
-        </ul>
+const Nav = () => (
+    <nav className="navbar">
+        <div className="navbar-left">
+            <div className="navbar-logo">
+                <img src="../assets/img/logoC.png" alt="Nav logo" />
+            </div>
+            <ul className="navbar-menu">
+                <NavLink name="Home" to="/" /> 
+                <NavLink name="Camps" to="/camps" /> 
+                <NavLink name="About" to="/about" /> 
+            </ul>
+        </div>
+
+        <div className="navbar-right">
+            <ul className="navbar-sign">
+                <li className="navbar-sign-item">
+                    <a href="/signin">Sign in</a>
+                </li>
+                <li className="navbar-sign-item">
+                    <a href="/signup">Sign up</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 );
 
