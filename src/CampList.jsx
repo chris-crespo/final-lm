2ebab19c9c20c3f8bd2594364c47de27f273b322
Chris <christian.crespoo98@gmail.com>
2022-05-14 14:27:02 +0200

    parse lang correctly

--- src/CampList.jsx
+++ src/CampList.jsx
@@ -42,25 +42,29 @@ const CampList = ({ kinds, langs }) => {
             })
     }, [])
 
+    const parseLangs = langs => Array.from(langs).map(lang => lang.textContent)
     const parseCamp = camp => Array.from(camp.children)
         .reduce((acc, child) => ({ 
             ...acc, 
-            [child.localName]: child.textContent 
+            [child.localName]: child.localName === "langs" 
+                ? parseLangs(child.children)
+                : child.textContent 
         }), { img: `../assets/img/camp-card${randBetween(1, 8)}.jpg` })
 
     const [search, setSearch] = useState("")
     const handleSearchBar = e => setSearch(e.target.value)
 
     const byName = curry((name, camp) => camp.name.toLowerCase().includes(name))
-    const emptyOrSameProp = curry((prop, opts, camp) =>
-        opts.length === 0 || opts.find(opt => opt.name === camp[prop]))
-    const byKind = emptyOrSameProp("kind", kinds);
-    const byLang = emptyOrSameProp("lang", langs);
+    const emptyOr = curry((thunk, opts, camp) => opts.length === 0 || thunk(camp))
+
+    const sameNameAs = curry((n1, { name }) => n1 === name)
+    const byKind = emptyOr(camp => kinds.find(sameNameAs(camp.kind)))
+    const byLang = emptyOr(camp => camp.langs.some(lang => langs.find(sameNameAs(lang))))
 
     useEffect(() => {
         // We could query from the database again, but we opt for this implementation because of 
         // its simplicity and because we have little time left.
-        setFilteredCamps(camps.filter(and(byName(search), byKind, byLang)))
+        setFilteredCamps(camps.filter(and(byName(search), byKind(kinds), byLang(langs))))
     }, [kinds, langs, search]);
 
     return (
