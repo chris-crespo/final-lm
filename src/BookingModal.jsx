b117cb805274525293928bfc41bf317a0b7821ab
krs98 <christian.crespoo.98@gmail.com>
2022-05-18 11:09:44 +0000

    add functionality

--- src/BookingModal.jsx
+++ src/BookingModal.jsx
@@ -1,57 +1,102 @@
-const Infolayout = ({ firstname,lastname }) => 
-    (
-        <div className="kid-info">
-            <div className="kid-avatar">
+import useForm from './hooks/useForm.js'
+import useSessionStorage from './hooks/useSessionStorage.js'
+import { api } from './api.js'
+
+const { useState, useEffect, useRef } = React
+
+const Infolayout = ({ kid }) => (
+    <div className="kid-info">
+        <div className="kid-avatar">
+            <img src="../assets/img/kid-pic.jpg" alt="avatar" />
+        </div>
+        <div className="first-name">
+            <p>{ kid.firstName }</p>            
+        </div>
+        <div className="last-name">
+            <p>{ kid.lastName }</p>
+         </div>
+    </div>
+)
+
+const Addinglayout = () => {
+    const [visible, setVisible] = useState(false);
+    const { 
+        register,
+        handleSubmit,
+        watch
+    } = useForm({ dni: "", firstName: "", lastName: "", age: "" });
+
+    const button = useRef(null);
+    useEffect(() => {
+        const listener = ({ valid }) => button.current.disabled = !valid;
+        const sub = watch(listener);
+        return () => sub.unsub();
+    }, []);
+
+    const [user] = useSessionStorage("user")
+    const onSubmit = kid => {
+        fetch(`${api}/kid/${user.email}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(kid)
+        }).then(_ => setVisible(false));
+    }
+
+    return (
+        <form className="add-kid-section" onSubmit={handleSubmit(onSubmit)}>
+            <div className={visible ? 'element-hidden' : 'element-visible'} id="addButton">
+                <input onClick={() => setVisible(!visible)} id="plus-icon" type="image" src="../assets/img/plus-icon.png" alt="plus-icon" />        
+            </div>
+            <div className={visible ? 'element-visible' : 'element-hidden'} id="kid-avatar" >
                 <img src="../assets/img/kid-pic.jpg" alt="avatar" />
             </div>
-            <div className="first-name">
-                <p>{ firstname }</p>            
+            <div className={visible ? 'element-visible' : 'element-hidden'} id="IDsection" >
+                <input type="text" placeholder="DNI" autoFocus
+                    { ...register("dni", {
+                        required: true,
+                        pattern: /\d{8}[A-Z]/ 
+                    }) }/>
             </div>
-            <div className="last-name">
-                <p>{ lastname }</p>
-             </div>
-        </div>
-    )
-const Addinglayout = () => {
-    const [visible, setVisible] = React.useState(false);
-    return (
-            <div className="add-kid-section" >
-                <style>{`
-
-.element-visible { display: flex }
-.element-hidden { display: none }
-
-`}</style>
-                
-                <div className={visible ? 'element-hidden' : 'element-visible'} id="addButton">
-                        <input onClick={() => setVisible(!visible)} id="plus-icon" type="image" src="../assets/img/plus-icon.png" alt="plus-icon" />              
-                </div>
-                
-                    <div className={visible ? 'element-visible' : 'element-hidden'} id="kid-avatar" >
-                        <img src="../assets/img/kid-pic.jpg" alt="avatar" />
-                    </div>
-                    <div className={visible ? 'element-visible' : 'element-hidden'} id="IDsection" >
-                        <input type="text" placeholder="DNI" />
-                    </div>
-                    <div className={visible ? 'element-visible' : 'element-hidden'} id="complete-name">
-                        <input type="text" placeholder="First Name" />
-                        <input type="text" placeholder="Last Name" />
-                    </div>
-                    <div className={visible ? 'element-visible' : 'element-hidden'} id="age">
-                        <input type="text" placeholder="Age" />
-                    </div>
-                    <button className={visible ? 'element-visible' : 'element-hidden'}>Add kid</button>
+            <div className={visible ? 'element-visible' : 'element-hidden'} id="complete-name">
+                <input type="text" placeholder="First Name" { ...register("firstName", {
+                    required: true
+                }) } />
+                <input type="text" placeholder="Last Name" { ...register("lastName", {
+                    required: true
+                }) } />
             </div>
+            <div className={visible ? 'element-visible' : 'element-hidden'} id="age">
+                <input type="text" placeholder="Age" { ...register("age", {
+                    required: true,
+                    pattern: /[4-9]|1[0-6]/
+                }) } />
+            </div>
+            <button className={visible ? 'element-visible' : 'element-hidden'} ref={button}>Add kid</button>
+        </form>
     );
 };
 
-const InfoCards = () => 
-     (
-                <div className="kid-cards">
-                    <Infolayout firstname="Juan" lastname="Perez" />
-                    <Infolayout firstname="Adrian" lastname="Perez" />
-                </div>
+const InfoCards = () => {
+    const [user] = useSessionStorage("user")
+    const [kids, setKids] = useState(null);
+    useEffect(() => {
+        if (user) fetch(`${api}/kids/${user.email}`)
+            .then(res => res.json())
+            .then(setKids);
+    }, [user])
+
+    useEffect(() => {
+        console.log(kids)
+    }, [kids]);
+
+    return (
+        <div className="kid-cards">
+            {
+                kids?.map(kid => <Infolayout kid={kid} />)
+            }
+        </div>
     )     
+}
 
 const AddCard = () => (
     <div className="add-kid-card">
@@ -60,19 +105,14 @@ const AddCard = () => (
 )
 
 
-const BookingModal = () => {
-    const [visible, setVisible] = React.useState(true);
-    return (
-    <div className={visible ? 'element-visible' : 'element-hidden'} id="booking-modal">
+const BookingModal = ({ show, close }) => (
+    <div className={show ? 'element-visible' : 'element-hidden'} id="booking-modal">
         <div className="close-button">
-            <button onClick={() => setVisible(!visible)}>X</button>
+            <button onClick={close}>X</button>
         </div>
         <InfoCards />
         <AddCard />
     </div>
-    )
-    
-}
-const container = document.querySelector("#booking-modal-wrapper");
-ReactDOM.createRoot(container).render(<BookingModal />);
-// export default BookingModal;
+)
+
+export default BookingModal;
